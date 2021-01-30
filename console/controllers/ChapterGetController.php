eb020b862d74bc476960c20c1d626f4b19359624
yakninja <andrey@yakninja.pro>
2021-01-30 20:21:05 +0100

    re-get temp error chapters

--- console/controllers/ChapterGetController.php
+++ console/controllers/ChapterGetController.php
@@ -29,13 +29,17 @@ class ChapterGetController extends Controller
         $chapter_ids = Chapter::find()
             ->select('id')
             ->where('(status = :new OR updated_at < :time) 
-                AND status <> :waiting AND status <> :export AND status <> :error',
+                AND status <> :waiting AND status <> :export 
+                AND (status <> :error OR error_message = :error1 OR error_message = :error2)',
                 [
                     'new' => Chapter::STATUS_NEW,
                     'waiting' => Chapter::STATUS_WAITING,
                     'export' => Chapter::STATUS_PUBLICATION,
                     'error' => Chapter::STATUS_ERROR,
                     'time' => time() - Yii::$app->params['chapter.getTimeout'],
+                    // these are temporary
+                    'error1' => 'Internal error encountered.',
+                    'error2' => 'The service is currently unavailable.',
                 ])
             ->createCommand()
             ->queryColumn();
